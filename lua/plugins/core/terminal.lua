5c2b86647dffabaf074f245a00ab3a9ebec729d9
initsyscall <initsyscall@gmail.com>
2026-08-20 00:01:39 +0530

    Feat: Dim Mode Toggle

--- lua/plugins/core/terminal.lua
+++ lua/plugins/core/terminal.lua
@@ -3,6 +3,7 @@ return {
   opts = {
     terminal = { enabled = true },
     zen = { enabled = true },
+    dim = { enabled = true, scope = { min_size = 5, max_size = 20 } },
     styles = {
       terminal = {
         bo = { filetype = "snacks_terminal" },
@@ -36,6 +37,25 @@ return {
     { "<leader>tf", function() require("snacks").terminal.open(nil, { win = { position = "float" } }) end,            desc = "Terminal (Float)" },
     { "<leader>gg", function() require("snacks").lazygit() end,                                                       desc = "Lazygit" },
     { "<leader>ty", function() require("snacks").terminal.toggle("yazi", { win = { style = "terminal_float" } }) end, desc = "Yazi" },
-    { "<leader>z",  function() require("snacks").zen() end,                                                     desc = "Zen Mode" },
+    { "<leader>z",  desc = "Zen" },
+    { "<leader>zt", function() require("snacks").zen() end,                                                     desc = "True Zen Mode" },
+    { "<leader>zd", function()
+      local dim = require("snacks").dim
+      local notify = function(msg)
+        local Snacks = package.loaded["snacks"]
+        if Snacks then
+          Snacks.notify.info(msg)
+        else
+          vim.notify(msg)
+        end
+      end
+      if dim.enabled then
+        dim.disable()
+        notify("Dim Off")
+      else
+        dim.enable()
+        notify("Dim On")
+      end
+    end, desc = "Toggle Zen Dim" },
   }
 }
